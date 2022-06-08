bccca83fd3509d8906dad362252d9eae05f9a412
catnguyendjv <cat.nguyen@drjoy.vn>
2022-06-09 02:42:48 +0900

    add params to get scenarios api

--- controllers/scenarios-controller.js
+++ controllers/scenarios-controller.js
@@ -3,9 +3,10 @@ import {HTTP_STATUS} from '../utils/constant.js';
 
 export const getScenarios = async (req, res) => {
     try {
-        const scenarios = await ScenarioModel.find();
+        const scenarios = await ScenarioModel.find().skip(req.query.page * req.query.size).limit(req.query.size);
+        const totalCount = await ScenarioModel.count();
         console.log(`Get all scenarios\nFind success! ${scenarios.length} records`);
-        res.status(HTTP_STATUS.OK).json(scenarios);
+        res.status(HTTP_STATUS.OK).json({scenarios: scenarios, totalCount: totalCount});
     } catch (err) {
         res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({error: err});
     }
