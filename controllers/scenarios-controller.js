206d858a81d280409cc2e97a9b4ecab13ce85e9c
tienLM_vn <tien.luu@drjoy.vn>
2022-06-20 13:45:40 +0900

    update 2

--- controllers/scenarios-controller.js
+++ controllers/scenarios-controller.js
@@ -4,15 +4,21 @@ import {HTTP_STATUS} from "../utils/constant.js";
 export const getScenarios = async (req, res) => {
   try {
     let searchQuery = {};
-    if (req.body.name !== "") {
-      searchQuery.name = {$regex: req.body.name, $options: "i"};
+    const searchName = req.query.name;
+    const searchTitle = req.query.title;
+    if (searchName !== "" && typeof searchName !== 'undefined') {
+      searchQuery.name = {$regex: searchName, $options: "i"};
     }
 
-    if (req.body.title !== "") {
-      searchQuery.title = {$regex: req.body.title, $options: "i"};
+    if (searchTitle !== "" && typeof searchTitle !== 'undefined') {
+      searchQuery.title = {$regex: searchTitle, $options: "i"};
     }
 
-    const scenarios = await ScenarioModel.find(searchQuery, null, {limit: req.body.size}).exec();
+    const scenarios = await ScenarioModel.find(
+      searchQuery,
+      null,
+      {skip:req.query.page * req.query.size, limit: req.query.size}
+    ).exec();
     
     const totalCount = await ScenarioModel.find(searchQuery).count();
     console.log(`Get all scenarios\nFind success! ${scenarios.length} records`);
